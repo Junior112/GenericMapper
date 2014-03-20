e6de05e8bb1fce411e15e51926aeb931cd0d136c
Junior112 <elfego.guerra@gmail.com>
2014-03-20 01:51:46 -0600

    clear wrong code

--- ADOTest/Entities/LINEA.cs
+++ ADOTest/Entities/LINEA.cs
@@ -11,12 +11,18 @@ namespace ADOTest.Entities
             FACTURALINEA = new List<FACTURA_LINEA>();
         }
         public virtual int Id { get; set; }
-        [GenericMapper.Classes.Map("PRODUCTO", nameRelation: "LineaProducto")]
+        
+        [GenericMapper.Classes.Map("PRODUCTO", true, nameRelation: "LineaProducto")]
         public virtual PRODUCTO PRODUCTO { get; set; }
-        public virtual double Cantidad { get; set; }
+
+        [GenericMapper.Classes.Map("Cantidad")]
+        public virtual double Cantidad2 { get; set; }
+        
         public virtual int IdProducto { get; set; }
 
+        [GenericMapper.Classes.Map("ALBARANLINEA", true, nameRelation: "LineaLineaAlbaran")]
         public virtual List<ALBARAN_LINEA> ALBARANLINEA { get; set; }
+        
         public virtual List<FACTURA_LINEA> FACTURALINEA { get; set; }
     }
 }
