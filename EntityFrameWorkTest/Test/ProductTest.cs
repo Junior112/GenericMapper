ff0bfaca8782834dc44e909c46ff83a58e8be359
Ernesto Herrera Salinas <ernestohs@gmail.com>
2015-10-08 12:52:17 -0500

    Update ProductTest.cs

--- EntityFrameWorkTest/Test/ProductTest.cs
+++ EntityFrameWorkTest/Test/ProductTest.cs
@@ -1,15 +1,39 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
 using EntityFrameWorkTest.DTOs;
 using GenericMapper.Interfaces;
 using NUnit.Framework;
 using GenericMapper.Extensions;
 using System.Linq;
+using System.Data.Common;
 
 namespace EntityFrameWorkTest.Test
 {
     [TestFixture]
     public class ProductTest
     {
+        // TODO: Create a base class for all the integration fixtures
+        [SetUp]
+        public void TestSetUp() 
+        {
+            var executeIntegrationTests = true;
+            try{
+                var builder = new DbConnectionStringBuilder
+                {
+                    ConnectionString = ConfigurationManager.ConnectionStrings["Test"].ConnectionString
+                };
+            } catch(ArgumentException ex){
+                Console.Error.WriteLine(ex.Message);
+                executeIntegrationTests = false; // Invalid connection string
+            }
+            
+             if (executeIntegrationTests)
+             {
+                 Assert.Ignore( "Local SQL Express is not setup.  Omitting integration fixture." );
+             }
+        }
+        
         [Test]
         public void Map_A_Product()
         {
